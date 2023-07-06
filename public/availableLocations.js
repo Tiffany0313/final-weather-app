6c6604c7eba3d68cf5aad5258a8e0306d60686bc
Tiffany0313 <pinky95123@gmail.com>
2023-07-06 17:20:45 +0800

    fix new taipei city weather

--- public/availableLocations.js
+++ public/availableLocations.js
@@ -1,52 +1,3 @@
-// STEP 1：匯入日出日落資料
-import sunriseAndSunsetData from './sunrise-sunset.json';
-
-export const getMoment = (locationName) => {
-    // STEP 2：從日出日落時間中找出符合的地區
-    const location = sunriseAndSunsetData.find(
-        (data) => data.locationName === locationName
-    );
-
-    // STEP 3：找不到的話則拋出錯誤訊息
-    if (!location) {
-        throw new Error(`找不到 ${location} 的日出日落資料`);
-    }
-
-    // STEP 4：取得當前時間
-    const now = new Date();
-
-    // STEP 5：將當前時間以 "2019-10-08" 的時間格式呈現
-    const nowDate = Intl.DateTimeFormat('zh-TW', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-    })
-        .format(now)
-        .replace(/\//g, '-');
-
-    // STEP 6：從該地區中找到對應的日期
-    const locationDate = location?.time.find((time) => time.dataTime === nowDate);
-
-    // STEP 7：找不到的話則拋出錯誤訊息
-    if (!locationDate) {
-        throw new Error(`找不到 ${locationName} 在 ${nowDate} 的日出日落資料`);
-    }
-
-    // STEP 8：將日出日落以及當前時間轉成時間戳記（TimeStamp）
-    const sunriseTimestamp = new Date(
-        `${locationDate.dataTime} ${locationDate.sunrise}`
-    ).getTime();
-    const sunsetTimestamp = new Date(
-        `${locationDate.dataTime} ${locationDate.sunset}`
-    ).getTime();
-    const nowTimeStamp = now.getTime();
-
-    // STEP 9：若當前時間介於日出和日落中間，則表示為白天，否則為晚上
-    return sunriseTimestamp <= nowTimeStamp && nowTimeStamp <= sunsetTimestamp
-        ? 'day'
-        : 'night';
-};
-
 export const availableLocations = [
     {
         cityName: '宜蘭縣',
@@ -70,7 +21,7 @@ export const availableLocations = [
     },
     {
         cityName: '雲林縣',
-        locationName: '國一N234K',
+        locationName: '雲林',
         sunriseCityName: '雲林縣',
     },
     {
@@ -115,12 +66,12 @@ export const availableLocations = [
     },
     {
         cityName: '臺南市',
-        locationName: '臺南',
+        locationName: '南區中心',
         sunriseCityName: '臺南市',
     },
     {
         cityName: '南投縣',
-        locationName: '國三N223K',
+        locationName: '日月潭',
         sunriseCityName: '南投縣',
     },
     {
@@ -154,7 +105,3 @@ export const availableLocations = [
         sunriseCityName: '新北市',
     },
 ];
-
-export const findLocation = (cityName) => {
-    return availableLocations.find((location) => location.cityName === cityName);
-};
